4a9fbbef739ba43d95ef3d71644f9f144f7a1eeb
RimshaRiaz-22 <rimshanimo22@gmail.com>
2022-12-18 16:15:22 +0500

    final chnges of membership req

--- apis/users/update-user.js
+++ apis/users/update-user.js
@@ -4,38 +4,124 @@ const { usersModel } = require('../../schemas')
 
 const UpdateUser = app.put('/', (req, res) => {
     const UserId =req.body._id
+    const userType = req.body.userType;
 
     usersModel.findById(UserId, (error, result) => {
         if (error) {
             res.send(error)
         } else {
             // res.send(result)
-            const updateData = {
-                email: req.body.email,
-                name: req.body.name,
-                dob: req.body.dob,
-                profession: req.body.profession,
-                city: req.body.city,
-                userType: req.body.userType,
-                DistrictArea:req.body.DistrictArea,
-                PPArea:req.body.PPArea,
-                UCArea:req.body.UCArea,
-                UnitArea:req.body.UnitArea,
-                gender: req.body.gender,
-                forum: req.body.forum,
-                role: req.body.role,
-                department: req.body.department,
-            }
-            const options = {
-                new: true
-            }
-            usersModel.findByIdAndUpdate(req.body._id, updateData, options, (error, result) => {
-                if (error) {
-                    res.send(error)
-                } else {
-                    res.send(result)
+            if (userType == 'District') {
+                const updateData = {
+                    email: req.body.email,
+                    name: req.body.name,
+                    dob: req.body.dob,
+                    profession: req.body.profession,
+                    city: req.body.city,
+                    userType: req.body.userType,
+                    DistrictArea:req.body.DistrictArea,
+                    // PPArea:req.body.PPArea,
+                    // UCArea:req.body.UCArea,
+                    // UnitArea:req.body.UnitArea,
+                    gender: req.body.gender,
+                    forum: req.body.forum,
+                    role: req.body.role,
+                    department: req.body.department,
                 }
-            })
+                const options = {
+                    new: true
+                }
+                usersModel.findByIdAndUpdate(req.body._id, updateData, options, (error, result) => {
+                    if (error) {
+                        res.send(error)
+                    } else {
+                        res.send(result)
+                    }
+                })
+            } else if (userType == 'Province') {
+                const updateData = {
+                    email: req.body.email,
+                    name: req.body.name,
+                    dob: req.body.dob,
+                    profession: req.body.profession,
+                    city: req.body.city,
+                    userType: req.body.userType,
+                    DistrictArea:req.body.DistrictArea,
+                    PPArea:req.body.PPArea,
+                    // UCArea:req.body.UCArea,
+                    // UnitArea:req.body.UnitArea,
+                    gender: req.body.gender,
+                    forum: req.body.forum,
+                    role: req.body.role,
+                    department: req.body.department,
+                }
+                const options = {
+                    new: true
+                }
+                usersModel.findByIdAndUpdate(req.body._id, updateData, options, (error, result) => {
+                    if (error) {
+                        res.send(error)
+                    } else {
+                        res.send(result)
+                    }
+                })
+            } else if (userType == 'UC') {
+                const updateData = {
+                    email: req.body.email,
+                    name: req.body.name,
+                    dob: req.body.dob,
+                    profession: req.body.profession,
+                    city: req.body.city,
+                    userType: req.body.userType,
+                    DistrictArea:req.body.DistrictArea,
+                    PPArea:req.body.PPArea,
+                    UCArea:req.body.UCArea,
+                    // UnitArea:req.body.UnitArea,
+                    gender: req.body.gender,
+                    forum: req.body.forum,
+                    role: req.body.role,
+                    department: req.body.department,
+                }
+                const options = {
+                    new: true
+                }
+                usersModel.findByIdAndUpdate(req.body._id, updateData, options, (error, result) => {
+                    if (error) {
+                        res.send(error)
+                    } else {
+                        res.send(result)
+                    }
+                })
+            } else if (userType == 'Unit') {
+                const updateData = {
+                    email: req.body.email,
+                    name: req.body.name,
+                    dob: req.body.dob,
+                    profession: req.body.profession,
+                    city: req.body.city,
+                    userType: req.body.userType,
+                    DistrictArea:req.body.DistrictArea,
+                    PPArea:req.body.PPArea,
+                    UCArea:req.body.UCArea,
+                    UnitArea:req.body.UnitArea,
+                    gender: req.body.gender,
+                    forum: req.body.forum,
+                    role: req.body.role,
+                    department: req.body.department,
+                }
+                const options = {
+                    new: true
+                }
+                usersModel.findByIdAndUpdate(req.body._id, updateData, options, (error, result) => {
+                    if (error) {
+                        res.send(error)
+                    } else {
+                        res.send(result)
+                    }
+                })
+            } else {
+            }
+          
         }
     })
    
